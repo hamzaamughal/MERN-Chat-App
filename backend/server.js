d0a6029e38d79f86c9f26511e5f3d31e42013387
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-23 16:04:06 +0500

    message routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require('colors');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
